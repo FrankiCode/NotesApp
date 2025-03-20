663104ee86393b440498f3b758abb031bec54535
FrankiCode <jaceknauka@gmail.com>
2025-03-20 11:04:38 +0400

    .

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import NavBar from '../src/Component/Layout/NavBar'
+import Main from '../src/Component/Layout/Main'
+
+function App() {
+
+
+  return <div>
+      <NavBar/>
+      <Main/>
+  </div>
+}
+
+export default App
